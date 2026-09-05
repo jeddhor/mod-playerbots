1a5bdc245acd57ed2d8cf12717b7ac8f6f8851fc
Jason Tripp <jason.a.tripp@gmail.com>
2026-09-05 13:26:30 -0700

    feat(gather): report Gather/Train/Vendor/Mailbox in the rpg status dump

--- src/Bot/RandomPlayerbotMgr.cpp
+++ src/Bot/RandomPlayerbotMgr.cpp
@@ -2894,10 +2894,12 @@ void RandomPlayerbotMgr::PrintStats()
         LOG_INFO("playerbots", "Bots rpg status:");
         LOG_INFO("playerbots",
                  "    Idle: {}, Rest: {}, GoGrind: {}, GoCamp: {}, MoveRandom: {}, MoveNpc: {}, DoQuest: {}, "
-                 "TravelFlight: {}, OutdoorPvP: {}",
+                 "TravelFlight: {}, OutdoorPvP: {}, Gather: {}, Train: {}, Vendor: {}, Mailbox: {}",
                  rpgStatusCount[RPG_IDLE], rpgStatusCount[RPG_REST], rpgStatusCount[RPG_GO_GRIND],
                  rpgStatusCount[RPG_GO_CAMP], rpgStatusCount[RPG_WANDER_RANDOM], rpgStatusCount[RPG_WANDER_NPC],
-                 rpgStatusCount[RPG_DO_QUEST], rpgStatusCount[RPG_TRAVEL_FLIGHT], rpgStatusCount[RPG_OUTDOOR_PVP]);
+                 rpgStatusCount[RPG_DO_QUEST], rpgStatusCount[RPG_TRAVEL_FLIGHT], rpgStatusCount[RPG_OUTDOOR_PVP],
+                 rpgStatusCount[RPG_GATHER], rpgStatusCount[RPG_TRAIN], rpgStatusCount[RPG_VENDOR],
+                 rpgStatusCount[RPG_MAILBOX]);
 
         LOG_INFO("playerbots", "Bots total quests:");
         LOG_INFO("playerbots", "    Accepted: {}, Rewarded: {}, Dropped: {}", rpgStasticTotal.questAccepted,
